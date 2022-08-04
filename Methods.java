7122c817968e42f8f802dac840d54e706ee0d32f
Alfredo08 <alfredosv08@gmail.com>
2022-08-04 15:45:35 -0500

    Added spaces in Methods.java

--- Methods.java
+++ Methods.java
@@ -4,6 +4,9 @@ public class Methods {
     public static int addTwoNumbers( int num1, int num2 ){
         return num1 + num2;
     }
+
+
+    
     public static void main( String args[] ){
         int num1 = 10;
         int num2 = 20;
